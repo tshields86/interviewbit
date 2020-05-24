f363af4c5e38b1519fc2923a8ea2a672a10c62fb
Travis Shields <travis.shields@gmail.com>
2020-05-24 14:48:31 -0400

    Add countAndSay algo solution

---
+++ countAndSay.js
@@ -0,0 +1,27 @@
+/* https://www.interviewbit.com/problems/count-and-say/ */
+
+const generateValue = input => {
+  if (input.length === 1) return `1${input}`;
+
+  let result = '';
+  let current = input[0];
+  let count = 1;
+
+  for (let i = 0; i < input.length; i++) {
+    if (current !== input[i+1]) {
+      result += `${count}${current}`;
+      current = input[i + 1];
+      count = 1;
+    } else count++;
+  }
+
+  return result;
+};
+
+const countAndSay = n => {
+  let val = '1';
+  for (let i = 1; i <= n; i++) {
+    if (i === n) return val;
+    val = generateValue(val);
+  }
+};
